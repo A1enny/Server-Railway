7eef3e82ee7d2194ab456df31cc8747b485861f5
A1enny <85298819+A1enny@users.noreply.github.com>
2025-03-17 17:14:02 +0700

    aaaa

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 const inventoryController = require("../controllers/inventoryController");
 
-module.exports = (io) => {
-    router.get("/", inventoryController.getMaterials);
-    router.get("/:id", inventoryController.getMaterialById);
-    router.post("/", inventoryController.addMaterial);
-    router.post("/batch", inventoryController.addBatchMaterials);
-    router.delete("/:id", inventoryController.deleteMaterial);
+router.get("/", inventoryController.getMaterials);
+router.get("/:id", inventoryController.getMaterialById);
+router.post("/", inventoryController.addMaterial);
+router.delete("/:id", inventoryController.deleteMaterial);
 
-    return router;
-};
+module.exports = router;
